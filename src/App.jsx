510735f53c61c1c37bd03da05d6637798f3ca235
quntaoz <quntaozheng@gmail.com>
2024-05-24 23:55:16 -0400

    using react query with server

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 const App = () => {
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+  })
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const anecdotes = result.data
 
   return (
     <div>
